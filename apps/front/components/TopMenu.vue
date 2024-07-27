28220e87745f5edcaf1ae7d0cfa363ddcb0846ab
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-07-28 07:07:28 +0900

    add tree

--- apps/front/components/TopMenu.vue
+++ apps/front/components/TopMenu.vue
@@ -7,7 +7,7 @@
         v-for="tab in tabs"
         :key="tab.id"
         :class="[{'bg-secondary-100': currentTab == tab.id, 'opacity-60': currentTab != tab.id}, 'flex-1 py-1 text-center cursor-pointer']"
-        class="flex-1 bg-secondary-100 text-white rounded-t py-1 text-center cursor-pointer"
+        class="flex-1 bg-secondary-100 text-white rounded-xd py-1 text-center cursor-pointer"
         @click="selectTab(tab.id)"
       >
         {{ tab.title }}
@@ -23,8 +23,8 @@
   })
 
   const tabs = [
-    {id: 1, title: 'Article'},
-    {id: 2, title: 'Code'}
+    {id: 1, title: 'Edit'},
+    {id: 2, title: 'Preview'}
   ]
   const selectTab = (tabId: number) => {
     currentTab.value = tabId
--- apps/front/components/Window.vue
+++ apps/front/components/Window.vue
@@ -1,6 +1,8 @@
 <template>
-  <TopMenu v-model:currentTab="currentTab" />
-  <div class="flex flex-grow p-2">
+  <div class="w-1/6">
+    <TopMenu v-model:currentTab="currentTab" />
+  </div>
+  <div class="flex flex-grow p-2 py-5">
     <div v-if="currentTab === 1" class="flex h-full w-full">
       <div class="w-1/2 border-r">
         <Editor class="h-full w-full" />
@@ -10,10 +12,13 @@
       </div>
     </div>
     <div v-if="currentTab === 2" class="flex w-full h-full">
-      <div class="w-1/2 h-full border-r">
+      <div class="w-1/5 border-r">
+        <FileTree class="h-full w-full" />
+      </div>
+      <div class="w-2/5 h-full">
         <CodeEditor class="h-full w-full" />
       </div>
-      <div class="w-1/2 h-full">
+      <div class="w-2/5 h-full">
         <Preview class="h-full w-full px-2" />
       </div>
     </div>
